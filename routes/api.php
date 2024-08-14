469a84653412ceee637f3ae27594dddb9c99820f
Rachid AOui <aouirachid22@gmail.com>
2024-08-14 01:31:19 +0100

    we start creating the role and permission using spatie

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\PermissionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::resource('permissions',PermissionController::class);
